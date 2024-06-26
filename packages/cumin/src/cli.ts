4bd9bae6b338e0ded535b18af9b72c2f4491d320
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2024-06-26 03:14:12 -0400

    Add default cli route

---
+++ packages/cumin/src/cli.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+import figlet from "figlet";
+import Client from "@fnndsc/chrisapi";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+// Read package.json
+const packageJson = JSON.parse(
+  readFileSync(join(__dirname, "..", "package.json"), "utf-8"),
+);
+const version = packageJson.version;
+
+async function main() {
+  console.log(Client);
+  console.log(Client.getAuthToken);
+
+  console.log(figlet.textSync("cumin"));
+  console.log(" -- CUbe Management INterface --");
+  console.log("      == version ", version, "==");
+  console.log("\n");
+  console.log("Welcome to cumin! Also known as a spicy part of any chili.\n\n");
+  console.log(
+    "Note that cumin isn't really intended to be used as standalone program",
+  );
+  console.log(
+    "rather, it is a support interface providing useful services especially",
+  );
+  console.log("the ChILI project.");
+
+  // const chrisUrl = "http://localhost:8000/api/v1/";
+  // const usersUrl = chrisUrl + "users/";
+  // const authUrl = chrisUrl + "auth-token/";
+  // let authToken;
+  // let resp;
+  //
+  // console.log(Client);
+  // console.log(Client.getAuthToken);
+  //
+  // try {
+  //   // fetch a user auth token
+  //   resp = await Client.getAuthToken(authUrl, "cube", "cube1234");
+  //   console.log("Auth token:", resp);
+  // } catch (error) {
+  //   console.error("Error fetching auth token:", error);
+  // }
+}
+
+main().catch(console.error);

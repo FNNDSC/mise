acb49925b4d35cc3064be6142bcd83215e861af9
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2024-06-26 03:36:31 -0400

    Misc updates, perhaps superfluous?

--- packages/cumin/types/chrisapi.d.ts
+++ packages/cumin/types/chrisapi.d.ts
@@ -1,14 +1,20 @@
-declare module '@fnndsc/chrisapi' {
-  const Client: any;
-  export default Client;
+declare module "@fnndsc/chrisapi" {
+  class Client {
+    constructor(url: string, options?: { token: string });
 
-  // Explicitly declare the Feed class with any type
-  export class Feed {
-    tagFeed(tag_id: number, timeout?: number): Promise<any>;
-    // Add other methods if needed, all returning 'any'
+    static getAuthToken(
+      authUrl: string,
+      username: string,
+      password: string,
+      timeout?: number,
+    ): Promise<string>;
   }
 
-  // Declare any other exports from the module as 'any'
+  export default Client;
+
+  export class Feed {
+    tagFeed(tag_id: number, timeout?: number): Promise<any>;
+  }
+
   export const Tagging: any;
-  // Add any other exports you're using from the module
 }

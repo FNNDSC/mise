d4fac7cf13f45e5e08489882722a3571f6d94458
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-07-23 10:48:14 -0400

    WIP with in-line debug conveniences

--- packages/cumin/src/cli.ts
+++ packages/cumin/src/cli.ts
@@ -3,7 +3,14 @@ import figlet from "figlet";
 import Client from "@fnndsc/chrisapi";
 import { readFileSync } from "fs";
 import { join } from "path";
-import { chrisFileBrowser_create } from "./filebrowser/chrisFileBrowser";
+// import { chrisFileBrowser_create } from "./filebrowser/chrisFileBrowser";
+import { ChRISinode } from "./filebrowser/chrisFiles";
+
+// import { createChrisFilesGetFiles } from "./filebrowser/chrisFiles";
+import { ChRISinode_create } from "./filebrowser/chrisFiles";
+
+import { ChRISFeed } from "./feeds/chrisFeed";
+
 // Read package.json
 const packageJson = JSON.parse(
   readFileSync(join(__dirname, "..", "package.json"), "utf-8"),
@@ -14,10 +21,23 @@ async function main() {
   // console.log(Client);
   // console.log(Client.getAuthToken);
 
-  const chrisFileBrowser = await chrisFileBrowser_create(
-    "rudolphpienaar/uploads",
-  );
-  console.log(chrisFileBrowser);
+  const chrisFeed = new ChRISFeed();
+  const feedResults = await chrisFeed.asset.resourceFields_get();
+
+  // const chrisFileBrowser = await chrisFileBrowser_create(
+  //   "home/rudolphpienaar/uploads",
+  // );
+  // const chrisFileBrowser2 = await chrisFileBrowser_create(
+  //   "home/rudolphpienaar/uploads/mail",
+  // );
+  // const results = await chrisFileBrowser2?.resource?.resourceFields_get();
+
+  const chrisInode: ChRISinode|null = await ChRISinode_create("home/rudolphpienaar/uploads/mail");
+
+  if(chrisInode && chrisInode.fileBrowser) {
+    const filefields = await chrisInode.fileBrowser.resource?.resourceFields_get();
+  }
+
   console.log(figlet.textSync("cumin"));
   console.log(" -- CUbe Management INterface --");
   console.log("      == version ", version, "==");

e9615ddaa685192774560b8eb110df01e85f6b71
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-07-23 10:51:55 -0400

    Major refactorization and simplification

--- packages/cumin/src/filebrowser/chrisFileBrowser.ts
+++ packages/cumin/src/filebrowser/chrisFileBrowser.ts
@@ -10,19 +10,91 @@ interface ParamOptions {
   [key: string]: any;
 }
 
+export interface BoolString {
+  status: boolean;
+  message: string;
+}
+
+export enum BrowserType {
+  Files = "Files",
+  Links = "Links",
+  Dirs = "Dirs",
+};
+
 export class ChRISFileBrowser {
-  // private _client: Client | null;
-  private _path: string;
-  // private _chrisFileBrowserFolder: FileBrowserFolder | null = null;
+  private _client: Client | null;
+  private _chrisFileBrowserFolder: FileBrowserFolder | null = null;
   private _resource: ChRISResource | null = null;
 
-  constructor(path?: string) {
-    this._path = path || "";
-    // this._client = chrisConnection.getClient();
-    console.log("In ChRISFileBrowser, declaring new ChRISResource...");
+  constructor(variant: BrowserType = BrowserType.Files, chrisObj: FileBrowserFolder) {
+    this._client = chrisConnection.getClient();
+    if(!this._client) {
+      console.error("Could not access ChRIS. Have you connected with the 'connect' command?");
+      process.exit(1);
+    }
+    this._chrisFileBrowserFolder = chrisObj;
     this._resource = new ChRISResource();
-    chrisConnection.loggedIn_check();
-    // this._chrisFileBrowserFolder = null;
+  }
+
+  getFiles_bind(): BoolString {
+    let bindOp: BoolString = { "status": false, "message": "getFiles not bound"};
+    if (this._resource && this._chrisFileBrowserFolder) {
+      this._resource.resource_bindGetMethodToObj(
+        this._chrisFileBrowserFolder,
+        this._chrisFileBrowserFolder.getFiles,
+        BrowserType.Files
+      );
+      bindOp.status = true;
+      bindOp.message = "ChRISFileBrowser bound to fileBrowserFolder.getFiles";
+    }
+    return bindOp;
+  }
+
+  getLinks_bind(): BoolString {
+    let bindOp: BoolString = { "status": false, "message": "getLinks not bound"};
+    if (this._resource && this._chrisFileBrowserFolder) {
+      this._resource.resource_bindGetMethodToObj(
+        this._chrisFileBrowserFolder,
+        this._chrisFileBrowserFolder.getLinkFiles,
+        BrowserType.Links
+      );
+      bindOp.status = true;
+      bindOp.message = "ChRISFileBrowser bound to fileBrowserFolder.getLinkFiles";
+    }
+    return bindOp;
+  }
+
+  getDirs_bind(): BoolString {
+    let bindOp: BoolString = { "status": false, "message": "getDirs not bound"};
+    if (this._resource && this._chrisFileBrowserFolder) {
+      this._resource.resource_bindGetMethodToObj(
+        this._chrisFileBrowserFolder,
+        this._chrisFileBrowserFolder.getChildren,
+        BrowserType.Dirs
+      );
+      bindOp.status = true;
+      bindOp.message = "ChRISFileBrowser bound to fileBrowserFolder.getChildren";
+    }
+    return bindOp;
+  }
+
+  getMethod_bind(variant: BrowserType): BoolString {
+    let bindOp: BoolString = { "status": false, "message": ""};
+    if (!this._resource || !this._chrisFileBrowserFolder) {
+      bindOp.message = "ChRISFileBrowesr resource or fileBrowserFolder is null";
+      return bindOp;
+    }
+    bindOp.status = true;
+    switch(variant) {
+      case BrowserType.Files:
+        bindOp = this.getFiles_bind();
+        break;
+      case BrowserType.Links:
+        bindOp = this.getLinks_bind();
+      case BrowserType.Dirs:
+        bindOp = this.getDirs_bind();
+    }
+    return bindOp;
   }
 
   get resource(): ChRISResource | null {
@@ -30,103 +102,95 @@ export class ChRISFileBrowser {
   }
 
   get client(): Client | null {
-    if (this._resource) {
-      return this._resource.client;
+    if (this._client) {
+      return this._client;
     } else {
       return null;
     }
   }
 
-  get path(): string {
-    return this._path;
+  get chrisFileBrowserFolder(): FileBrowserFolder | null {
+    return this._chrisFileBrowserFolder;
   }
 
-  set path(path: string) {
-    this._path = path;
-  }
+  // async initialize(): Promise<FileBrowserFolder | null> {
+  //   console.log("In ChRISFileBrowser, starting to initialize...");
+  //   if (this.client) {
+  //     console.log("In ChRISFileBrowser, client OK... initialize...");
+  //     try {
+  //       if (this._resource) {
+  //         this._resource.resourceObj =
+  //           await this.client.getFileBrowserFolderByPath(this._path);
+  //         console.log("files...", await this._resource.resourceObj.getFiles());
+  //       }
+  //       console.log("In ChRISFileBrowser, this._resource.resourceObj set!");
+  //       console.log("In ChRISFileBrowser, initialize? ", this);
+  //       return this._resource?.resourceObj;
+  //     } catch (error) {
+  //       console.error("Error initializing ChRISFileBrowser: ", error);
+  //     }
+  //   } else {
+  //     console.error("this.client is null");
+  //   }
+  //   return null;
+  // }
 
-  get chrisFileBrowser(): FileBrowserFolder | null {
-    return this._resource?.resourceObj;
-  }
+  // bindGetMethod(
+  //   clientMethod?: (params: ParamOptions) => Promise<any>,
+  //   resourceName?: string,
+  // ): boolean {
+  //   let status: boolean = true;
+  //   console.log("Entering bindGetMethod");
+  //   console.log(this);
+  //   if (!this._resource?.resourceObj) {
+  //     return false;
+  //   }
+  //   console.log("Binding...");
+  //   try {
+  //     if (clientMethod) {
+  //       this._resource?.resource_bindGetMethodToObj(
+  //         this._resource,
+  //         clientMethod,
+  //       );
+  //       console.log("Binding to passed method", clientMethod);
+  //     } else {
+  //       this._resource?.resource_bindGetMethodToObj(
+  //         this._resource,
+  //         this._resource.resourceObj.getFiles,
+  //       );
+  //       console.log("Binding to (default) FileBrowserFolder getFiles");
+  //     }
+  //     if (resourceName && this._resource) {
+  //       this._resource.resourceName = resourceName;
+  //     }
+  //   } catch (error) {
+  //     console.error("getBind method failure: ", error);
+  //   }
+  //   return status;
+  // }
 
-  async initialize(): Promise<FileBrowserFolder | null> {
-    console.log("In ChRISFileBrowser, starting to initialize...");
-    if (this.client) {
-      console.log("In ChRISFileBrowser, client OK... initialize...");
-      try {
-        if (this._resource) {
-          this._resource.resourceObj =
-            await this.client.getFileBrowserFolderByPath(this._path);
-          console.log("files...", await this._resource.resourceObj.getFiles());
-        }
-        console.log("In ChRISFileBrowser, this._resource.resourceObj set!");
-        console.log("In ChRISFileBrowser, initialize? ", this);
-        return this._resource?.resourceObj;
-      } catch (error) {
-        console.error("Error initializing ChRISFileBrowser: ", error);
-      }
-    } else {
-      console.error("this.client is null");
-    }
-    return null;
-  }
-
-  bindGetMethod(
-    clientMethod?: (params: ParamOptions) => Promise<any>,
-    resourceName?: string,
-  ): boolean {
-    let status: boolean = true;
-    console.log("Entering bindGetMethod");
-    console.log(this);
-    if (!this._resource?.resourceObj) {
-      return false;
-    }
-    console.log("Binding...");
-    try {
-      if (clientMethod) {
-        this._resource?.resource_bindGetMethodToObj(
-          this._resource,
-          clientMethod,
-        );
-        console.log("Binding to passed method", clientMethod);
-      } else {
-        this._resource?.resource_bindGetMethodToObj(
-          this._resource,
-          this._resource.resourceObj.getFiles,
-        );
-        console.log("Binding to (default) FileBrowserFolder getFiles");
-      }
-      if (resourceName && this._resource) {
-        this._resource.resourceName = resourceName;
-      }
-    } catch (error) {
-      console.error("getBind method failure: ", error);
-    }
-    return status;
-  }
-
-  async initializeAndBind(): Promise<FileBrowserFolder | null> {
-    const fileBrowserFolder: FileBrowserFolder | null = await this.initialize();
-    if (!fileBrowserFolder) {
-      return null;
-    }
-    const bindStatusOK = this.bindGetMethod();
-    if (bindStatusOK) {
-      return fileBrowserFolder;
-    } else {
-      return null;
-    }
-  }
+  // async initializeAndBind(): Promise<FileBrowserFolder | null> {
+  //   const fileBrowserFolder: FileBrowserFolder | null = await this.initialize();
+  //   if (!fileBrowserFolder) {
+  //     return null;
+  //   }
+  //   const bindStatusOK = this.bindGetMethod();
+  //   if (bindStatusOK) {
+  //     return fileBrowserFolder;
+  //   } else {
+  //     return null;
+  //   }
+  // }
 }
 
-export async function chrisFileBrowser_create(
-  path: string = "",
-): Promise<ChRISFileBrowser | null> {
-  const browser = new ChRISFileBrowser(path);
-  const goodBinding = await browser.initializeAndBind();
-  if (goodBinding) {
-    return browser;
-  } else {
-    return null;
-  }
-}
+// export async function chrisFileBrowser_create(
+//   path: string = "",
+// ): Promise<ChRISFileBrowser | null> {
+//   const browser = new ChRISFileBrowser(path);
+//   const goodBinding = await browser.initializeAndBind();
+//   if (goodBinding) {
+//     return browser;
+//   } else {
+//     return null;
+//   }
+// }

5c5e5a59b50e60c3cb2b090256ccffa2f0299a25
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-07-23 10:50:49 -0400

    Remove singleton and better log in check logic

--- packages/cumin/src/feeds/chrisFeed.ts
+++ packages/cumin/src/feeds/chrisFeed.ts
@@ -1,6 +1,7 @@
 import Client from "@fnndsc/chrisapi";
-import { chrisConnection } from "../connect/chrisConnection.js";
-import { ChRISResource } from "../resources/chrisResources.js";
+import { FeedList } from "@fnndsc/chrisapi";
+import { ChRISConnection, chrisConnection } from "../connect/chrisConnection.js";
+import { ChRISResource, ListOptions } from "../resources/chrisResources.js";
 
 export class ChRISFeed {
   private client: Client | null;
@@ -8,6 +9,10 @@ export class ChRISFeed {
 
   constructor() {
     this.client = chrisConnection.getClient();
+    if(!this.client) {
+      console.error("Could not access ChRIS. Have you connected with the 'connect' command?");
+      process.exit(1);
+    }
     this._asset = new ChRISResource();
     if (this.client) {
       this._asset.resource_bindGetMethodToObj(
@@ -16,12 +21,9 @@ export class ChRISFeed {
       );
     }
     this._asset.resourceName = "Feeds";
-    this._asset.loggedIn_check();
   }
 
   get asset(): ChRISResource {
     return this._asset;
   }
 }
-
-export const chrisFeed = new ChRISFeed();

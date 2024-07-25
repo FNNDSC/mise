634bba2310203e05818482afa50a9538a4db33f6
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-07-25 13:10:33 -0400

    Improve filesystem handler

--- packages/chili/src/filesystem/filesystemHandler.ts
+++ packages/chili/src/filesystem/filesystemHandler.ts
@@ -4,35 +4,55 @@ import {
   ChRISinode_create,
   ListOptions,
   FilteredResourceData,
-  ResourcesByFields
+  ResourcesFromOptions,
+  ResourcesByFields,
+  BrowserType
 } from "@fnndsc/cumin";
 
 interface FSCLIoptions {
   page?: string;
-  fields?: string;
+  filefields?: string;
+  dirfields?: string;
+  linkfields?: string;
   [key: string]: any;
 }
 
 function optionsToParams(pluginOptions: FSCLIoptions): ListOptions {
   return {
     limit: pluginOptions.page ? parseInt(pluginOptions.page, 10) : 20,
-    offset: 0,
-    name: undefined,
-    fields: pluginOptions.fields,
+    offset: 0  
   };
 }
 
-async function listFileResources(options: FSCLIoptions): Promise<void> {
-  const chrisFiles: ChRISinode|null = await ChRISinode_create();
+async function listInodeResources(path: string, options: FSCLIoptions): Promise<void> {
+  const chrisInode: ChRISinode|null = await ChRISinode_create(path);
   const params: ListOptions = optionsToParams(options);
-  console.log("In listFileResources");
-  const results: FilteredResourceData =
-    await chrisFiles.inode.resource.resources_filterByFields(
-      await chrisFiles.inode.resource.resourceFields_get(
-        await chrisFiles.inode.resource.resources_getList(params),
-      ),
-    );
-  console.table(results.tableData, results.selectedFields);
+  const browserTypes: BrowserType[] = [BrowserType.Files, BrowserType.Links, BrowserType.Dirs];
+  const fieldOptions = {
+    [BrowserType.Files]: options.filefields,
+    [BrowserType.Links]: options.linkfields,
+    [BrowserType.Dirs]: options.dirfields
+  }
+  for(const browserType of browserTypes) {
+    const browser = chrisInode.getBrowser(browserType);
+    if(!browser || !browser._resource) {
+      console.error(`WARNING: ${browserType} browser or its resource is not available`);
+      continue;      
+    }
+    try {
+      const results: FilteredResourceData = 
+      await browser.resource.resources_filterByFields(
+        await browser.resource.resourceFields_get(
+          await browser.resource.resources_getList(params),
+          fieldOptions[browserType]
+        ),
+      );
+      console.log(`${browserType} resources:`)
+      console.table(results.tableData, results.selectedFields);
+    } catch (error) {
+      console.error(`${browserType} resources: not found`);
+    }
+  }
 }
 
 function fieldsList(inodeType: string, dataObj: ResourcesByFields): boolean {
@@ -48,10 +68,8 @@ function fieldsList(inodeType: string, dataObj: ResourcesByFields): boolean {
   return false;
 } 
 
-async function listFileResourceFields(): Promise<void> {
-  const chrisFiles: ChRISinode|null = await ChRISinode_create(
-    "home/rudolphpienaar/uploads",
-  );
+async function listInodeResourceFields(path: string = ""): Promise<void> {
+  const chrisFiles: ChRISinode|null = await ChRISinode_create(path);
   let fileFields: ResourcesByFields = await chrisFiles.fileBrowser.resource.resourceFields_get();
   fieldsList("file properties", fileFields);
   let linkFields: ResourcesByFields = await chrisFiles.linkBrowser.resource.resourceFields_get();
@@ -66,21 +84,29 @@ export async function setupFileBrowserCommand(program: Command): Promise<void> {
     .description("Interact with ChRIS inodes");
 
   pluginsCommand
-    .command("list")
+    .command("list [path]")
     .description("List filesystem elements")
     .option("-p, --page <size>", "Page size (default 20)")
     .option(
-      "-f, --fields <fields>",
-      "Comma-separated list of fields to display",
+      "-f, --filefields <fields>",
+      "Comma-separated list of fields to display for files",
     )
-    .action(async (options) => {
-      await listFileResources(options);
+    .option(
+      "-d, --dirfields <fields>",
+      "Comma-separated list of fields to display for dirs",
+    )
+    .option(
+      "-l, --linkfields <fields>",
+      "Comma-separated list of fields to display for links",
+    )
+    .action(async (path, options) => {
+      await listInodeResources(path, options);
     });
 
   pluginsCommand
-    .command("fieldslist")
+    .command("fieldslist [path]")
     .description("List the filebrowser resource fields")
-    .action(async () => {
-      await listFileResourceFields();
+    .action(async (path) => {
+      await listInodeResourceFields(path);
     });
 }

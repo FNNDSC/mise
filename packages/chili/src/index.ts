5af9160ca382179fc9c79408c6ffd56bff959eed
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-07-26 10:26:01 -0400

    New init for handlers

--- packages/chili/src/index.ts
+++ packages/chili/src/index.ts
@@ -4,30 +4,45 @@ import { Command } from "commander";
 import figlet from "figlet";
 import omelette from "omelette";
 
+import { setupConnectCommand } from "./connect/connectHandler.js";
+// import { listDir } from './chfs/chfs.js';
+import { FeedHandler } from "./feeds/feedHandler.js";
+import { PluginHandler } from "./plugins/pluginHandler.js";
+import { setupLfsCommand } from "./lfs/lfs.js";
+import { setupFileBrowserCommand } from "./filesystem/filesystemHandler.js";
+
 const program = new Command();
 
 console.log(figlet.textSync("ChILI"));
 console.log("The ChRIS Interactive Line Interface");
 
-import { setupConnectCommand } from "./connect/connectHandler.js";
-// import { listDir } from './chfs/chfs.js';
-import { setupFeedCommand } from "./feeds/feedHandler.js";
-import { setupPluginsCommand } from "./plugins/pluginHandler.js";
-import { setupLfsCommand } from "./lfs/lfs.js";
-import { setupFileBrowserCommand } from "./filesystem/filesystemHandler.js";
 program.version("1.0.0").description("A CLI for ChRIS");
 
 // Setup the subcommands
 setupConnectCommand(program);
 setupLfsCommand(program);
-setupFeedCommand(program);
-setupPluginsCommand(program);
 setupFileBrowserCommand(program);
+
+const pluginHandler = new PluginHandler();
+pluginHandler.setupCommand(program);
+
+const feedHandler = new FeedHandler();
+feedHandler.setupCommand(program);
 
 const completion = omelette(`chili|chili`);
 completion.tree({
   connect: ["--user", "--password"],
   lfs: ["ls", "mkdir", "touch"],
+  plugins: {
+    list: ["--page", "--fields", "--search"],
+    info: ["<pluginId>"],
+    fieldslist: [],
+  },
+  feeds: {
+    list: ["--page", "--fields", "--search"],
+    new: ["--name", "--path"],
+    fieldslist: [],
+  },
 });
 
 completion.init();

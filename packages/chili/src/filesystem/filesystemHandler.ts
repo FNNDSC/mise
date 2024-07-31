85dcfd1296ea42baf189687bdcceb054bb15fdd2
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-07-31 10:41:41 -0400

    Refactoring and prettier

--- packages/chili/src/filesystem/filesystemHandler.ts
+++ packages/chili/src/filesystem/filesystemHandler.ts
@@ -5,8 +5,9 @@ import {
   ListOptions,
   FilteredResourceData,
   ResourcesFromOptions,
+  optionsToParams,
   ResourcesByFields,
-  BrowserType
+  BrowserType,
 } from "@fnndsc/cumin";
 
 interface FSCLIoptions {
@@ -17,37 +18,54 @@ interface FSCLIoptions {
   [key: string]: any;
 }
 
-function optionsToParams(pluginOptions: FSCLIoptions): ListOptions {
-  return {
-    limit: pluginOptions.page ? parseInt(pluginOptions.page, 10) : 20,
-    offset: 0  
-  };
-}
+// function optionsToParams(pluginOptions: FSCLIoptions): ListOptions {
+//   return {
+//     limit: pluginOptions.page ? parseInt(pluginOptions.page, 10) : 20,
+//     offset: 0,
+//   };
+// }
 
-async function listInodeResources(path: string, options: FSCLIoptions): Promise<void> {
-  const chrisInode: ChRISinode|null = await ChRISinode_create(path);
-  const params: ListOptions = optionsToParams(options);
-  const browserTypes: BrowserType[] = [BrowserType.Files, BrowserType.Links, BrowserType.Dirs];
+async function listInodeResources(
+  path: string,
+  options: FSCLIoptions
+): Promise<void> {
+  const chrisInode: ChRISinode | null = await ChRISinode_create(path);
+  if (!chrisInode) {
+    console.error(`Could not find path '${path}' in the CUBE filesystem.`);
+    return;
+  }
+  const params: ListOptions = optionsToParams({
+    ...options,
+    returnFilter: "limit,offset",
+  });
+  console.log(params);
+  const browserTypes: BrowserType[] = [
+    BrowserType.Files,
+    BrowserType.Links,
+    BrowserType.Dirs,
+  ];
   const fieldOptions = {
     [BrowserType.Files]: options.filefields,
     [BrowserType.Links]: options.linkfields,
-    [BrowserType.Dirs]: options.dirfields
-  }
-  for(const browserType of browserTypes) {
+    [BrowserType.Dirs]: options.dirfields,
+  };
+  for (const browserType of browserTypes) {
     const browser = chrisInode.getBrowser(browserType);
-    if(!browser || !browser._resource) {
-      console.error(`WARNING: ${browserType} browser or its resource is not available`);
-      continue;      
+    if (!browser || !browser._resource) {
+      console.error(
+        `WARNING: ${browserType} browser or its resource is not available`
+      );
+      continue;
     }
     try {
-      const results: FilteredResourceData = 
-      await browser.resource.resources_filterByFields(
-        await browser.resource.resourceFields_get(
-          await browser.resource.resources_getList(params),
-          fieldOptions[browserType]
-        ),
-      );
-      console.log(`${browserType} resources:`)
+      const results: FilteredResourceData =
+        await browser.resource.resources_filterByFields(
+          await browser.resource.resourceFields_get(
+            await browser.resource.resources_getList(params),
+            fieldOptions[browserType]
+          )
+        );
+      console.log(`${browserType} resources:`);
       console.table(results.tableData, results.selectedFields);
     } catch (error) {
       console.error(`${browserType} resources: not found`);
@@ -56,25 +74,28 @@ async function listInodeResources(path: string, options: FSCLIoptions): Promise<
 }
 
 function fieldsList(inodeType: string, dataObj: ResourcesByFields): boolean {
-  if(!dataObj) {
+  if (!dataObj) {
     console.log("No " + inodeType + " at this path");
     return false;
   }
-  if(dataObj.fields) {
+  if (dataObj.fields) {
     console.log(inodeType);
     console.table(dataObj.fields);
     return true;
   }
   return false;
-} 
+}
 
 async function listInodeResourceFields(path: string = ""): Promise<void> {
-  const chrisFiles: ChRISinode|null = await ChRISinode_create(path);
-  let fileFields: ResourcesByFields = await chrisFiles.fileBrowser.resource.resourceFields_get();
+  const chrisFiles: ChRISinode | null = await ChRISinode_create(path);
+  let fileFields: ResourcesByFields =
+    await chrisFiles.fileBrowser.resource.resourceFields_get();
   fieldsList("file properties", fileFields);
-  let linkFields: ResourcesByFields = await chrisFiles.linkBrowser.resource.resourceFields_get();
-  fieldsList("link properties", linkFields)
-  let dirFields: ResourcesByFields = await chrisFiles.dirBrowser.resource.resourceFields_get();
+  let linkFields: ResourcesByFields =
+    await chrisFiles.linkBrowser.resource.resourceFields_get();
+  fieldsList("link properties", linkFields);
+  let dirFields: ResourcesByFields =
+    await chrisFiles.dirBrowser.resource.resourceFields_get();
   fieldsList("dir properties", dirFields);
 }
 
@@ -89,15 +110,15 @@ export async function setupFileBrowserCommand(program: Command): Promise<void> {
     .option("-p, --page <size>", "Page size (default 20)")
     .option(
       "-f, --filefields <fields>",
-      "Comma-separated list of fields to display for files",
+      "Comma-separated list of fields to display for files"
     )
     .option(
       "-d, --dirfields <fields>",
-      "Comma-separated list of fields to display for dirs",
+      "Comma-separated list of fields to display for dirs"
     )
     .option(
       "-l, --linkfields <fields>",
-      "Comma-separated list of fields to display for links",
+      "Comma-separated list of fields to display for links"
     )
     .action(async (path, options) => {
       await listInodeResources(path, options);

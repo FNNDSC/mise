5ddf7cbce12ddc16ca1fd21453d01a138521873f
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-08-01 10:14:18 -0400

    New extension pattern

--- packages/cumin/src/plugins/chrisPlugins.ts
+++ packages/cumin/src/plugins/chrisPlugins.ts
@@ -1,62 +1,88 @@
 import Client from "@fnndsc/chrisapi";
 import { chrisConnection } from "../connect/chrisConnection";
-import { ChRISResource, ListOptions, FilteredResourceData } from "../resources/chrisResources";
-import { ChRISElementsGet, QueryHits, optionsToParams, extractRecordToQueryHits } from "../utils/keypair";
+import {
+  ChRISResource,
+  ListOptions,
+  FilteredResourceData,
+} from "../resources/chrisResources";
+import { ChRISResourceGroup } from "../resources/chrisResourceGroup";
+import {
+  ChRISElementsGet,
+  QueryHits,
+  optionsToParams,
+  extractRecordToQueryHits,
+} from "../utils/keypair";
 
-export class ChRISPluginGroup {
-  private _client: Client | null;
-  private _asset: ChRISResource;
-
+export class ChRISPluginGroup extends ChRISResourceGroup {
   constructor() {
-    this._client = chrisConnection.getClient();
-    if(!this._client) {
-      console.error("Could not access ChRIS. Have you connected with the 'connect' command?");
-      process.exit(1);
-    }
-    this._asset = new ChRISResource();
-    if (this._client) {
-      this._asset.resource_bindGetMethodToObj(
-        this._client,
-        this._client.getPlugins,
-      );
-    }
-    this._asset.resourceName = "Plugins";
-  }
-
-  public get client(): Client | null {
-    return this._client
-  }
-
-  get asset(): ChRISResource {
-    return this._asset;
+    super("Plugins", "getPlugins");
   }
 }
+
+// export class ChRISPluginGroup {
+//   private _client: Client | null;
+//   private _asset: ChRISResource;
+
+//   constructor() {
+//     this._client = chrisConnection.getClient();
+//     if (!this._client) {
+//       console.error(
+//         "Could not access ChRIS. Have you connected with the 'connect' command?"
+//       );
+//       process.exit(1);
+//     }
+//     this._asset = new ChRISResource();
+//     if (this._client) {
+//       this._asset.resource_bindGetMethodToObj(
+//         this._client,
+//         this._client.getPlugins
+//       );
+//     }
+//     this._asset.resourceName = "Plugins";
+//   }
+
+//   public get client(): Client | null {
+//     return this._client;
+//   }
+
+//   get asset(): ChRISResource {
+//     return this._asset;
+//   }
+// }
 
 export class ChRISPlugin {
   private _client: Client | null;
 
   constructor() {
     this._client = chrisConnection.getClient();
-    if(!this._client) {
-      console.error("Could not access ChRIS. Have you connected with the 'connect' command?");
+    if (!this._client) {
+      console.error(
+        "Could not access ChRIS. Have you connected with the 'connect' command?"
+      );
       process.exit(1);
     }
   }
 
-  async pluginHits_get(searchOptions: ChRISElementsGet): Promise<QueryHits | null> {
+  async pluginHits_get(
+    searchOptions: ChRISElementsGet
+  ): Promise<QueryHits | null> {
     const chrisPluginGroup = new ChRISPluginGroup();
     const searchParams: ListOptions = optionsToParams(searchOptions);
-    const searchResults: FilteredResourceData | null = 
-      await chrisPluginGroup.asset.resources_listAndFilterByOptions(searchParams);
-    if(!searchResults) {
+    const searchResults: FilteredResourceData | null =
+      await chrisPluginGroup.asset.resources_listAndFilterByOptions(
+        searchParams
+      );
+    if (!searchResults) {
       return null;
     }
-    const queryHits: QueryHits = extractRecordToQueryHits(searchResults.tableData, "id");
+    const queryHits: QueryHits = extractRecordToQueryHits(
+      searchResults.tableData,
+      "id"
+    );
     return queryHits;
   }
 
-  async pluginIDs_get(name_exact: string): Promise<QueryHits|null> {
-    return await this.pluginHits_get({"search": "name_exact: " + name_exact})
+  async pluginIDs_get(name_exact: string): Promise<QueryHits | null> {
+    return await this.pluginHits_get({ search: "name_exact: " + name_exact });
   }
-
 }

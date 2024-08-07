e95d438da52974af06439e46ed91e33fb1888c3a
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-08-07 19:06:57 -0400

    Add some useful logging

--- packages/cumin/src/cli.ts
+++ packages/cumin/src/cli.ts
@@ -5,6 +5,8 @@ import { readFileSync } from "fs";
 import { join } from "path";
 // import { chrisFileBrowser_create } from "./filebrowser/chrisFileBrowser";
 import { ChRISinode } from "./filebrowser/chrisFiles";
+import { chrisConnection } from "./connect/chrisConnection";
+import { connectionConfig } from "./config/config";
 
 // import { createChrisFilesGetFiles } from "./filebrowser/chrisFiles";
 import { ChRISinode_create } from "./filebrowser/chrisFiles";
@@ -13,24 +15,32 @@ import { ChRISFeedGroup } from "./feeds/chrisFeed";
 
 // Read package.json
 const packageJson = JSON.parse(
-  readFileSync(join(__dirname, "..", "package.json"), "utf-8"),
+  readFileSync(join(__dirname, "..", "package.json"), "utf-8")
 );
 const version = packageJson.version;
 
 async function main() {
-
   console.log(figlet.textSync("cumin"));
   console.log(" -- CUbe Management INterface --");
   console.log("      == version ", version, "==");
   console.log("\n");
-  console.log("Welcome to cumin! Also known as a spicy part of any chili.\n\n");
   console.log(
-    "Note that cumin isn't really intended to be used as standalone program;",
+    "Welcome to cumin! Also known as the basis for any good chili.\n\n"
   );
   console.log(
-    "rather, it is a support interface providing useful services especially",
+    "Note that cumin isn't really intended to be used as standalone program;"
+  );
+  console.log(
+    "rather, it is a support interface providing useful services especially"
   );
   console.log("the ChILI project.");
+  console.log("\n");
+  console.log(
+    "For this set of configuration values read during initialization,"
+  );
+  console.log("the authorization token is: ", chrisConnection.getAuthToken());
+  console.log("the chris URL is ", chrisConnection.getChRISurl());
+  console.log(connectionConfig);
 }
 
 main().catch(console.error);

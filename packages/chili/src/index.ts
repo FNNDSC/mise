58cfbbb996b076c8604989663899987d804a38df
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-08-08 20:11:12 -0400

    Major refactor for bootstrapping fix and also add preHook for --context

--- packages/chili/src/index.ts
+++ packages/chili/src/index.ts
@@ -5,87 +5,98 @@ import figlet from "figlet";
 import omelette from "omelette";
 
 import { setupConnectCommand } from "./connect/connectHandler.js";
-// import { listDir } from './chfs/chfs.js';
 import { FeedGroupHandler, FeedMemberHandler } from "./feeds/feedHandler.js";
 import { PluginGroupHandler } from "./plugins/pluginHandler.js";
 import { PluginMetaGroupHandler } from "./plugins/pluginMetaHandler.js";
-import { FileGroupHandler } from "./filesystem/fileGroupHandler.js";
 import { setupInodeCommand } from "./filesystem/inodeCommand.js";
-import { setupLfsCommand } from "./lfs/lfs.js";
 import { setupFileBrowserCommand } from "./filesystem/filesystemHandler.js";
+import { chrisConnection } from "@fnndsc/cumin";
 
 const program = new Command();
 
 program.version("1.0.0").description("A CLI for ChRIS");
 
-// Setup the subcommands
-setupConnectCommand(program);
-setupLfsCommand(program);
-setupFileBrowserCommand(program);
-setupInodeCommand(program);
-
-const pluginGroupHandler = new PluginGroupHandler();
-pluginGroupHandler.setupCommand(program);
-
-const pluginMetaGroupHandler = new PluginMetaGroupHandler();
-pluginMetaGroupHandler.setupCommand(program);
-
-const feedGroupHandler = new FeedGroupHandler();
-feedGroupHandler.setupCommand(program);
-
-const feedMemberHandler = new FeedMemberHandler();
-feedMemberHandler.setupCommand(program);
-
-// const fileGroupHandler = await FileGroupHandler.create(
-//   "/home/rudolphpienaar/uploads/mail"
-// );
-// fileGroupHandler.setupCommand(program);
-
-const completion = omelette(`chili|chili`);
-completion.tree({
-  connect: ["--user", "--password"],
-  lfs: ["ls", "mkdir", "touch"],
-  plugins: {
-    list: ["--page", "--fields", "--search"],
-    info: ["<pluginId>"],
-    fieldslist: [],
-    delete: ["<IDs>", "--search", "--force"],
-  },
-  feeds: {
-    list: ["--page", "--fields", "--search"],
-    new: ["--name", "--path"],
-    fieldslist: [],
-    delete: ["<IDs>", "--search", "--force"],
-  },
-  inode: {
-    "<path>": {
-      files: {
-        list: ["--page", "--fields", "--search"],
-        fieldslist: [],
-        delete: ["<IDs>", "--search", "--force"],
-        share: ["--force"],
-      },
-      file: {
-        view: ["--type"],
+function setupCommandCompletion() {
+  const completion = omelette(`chili|chili`);
+  completion.tree({
+    connect: ["--user", "--password"],
+    lfs: ["ls", "mkdir", "touch"],
+    plugins: {
+      list: ["--page", "--fields", "--search"],
+      info: ["<pluginId>"],
+      fieldslist: [],
+      delete: ["<IDs>", "--search", "--force"],
+    },
+    feeds: {
+      list: ["--page", "--fields", "--search"],
+      new: ["--name", "--path"],
+      fieldslist: [],
+      delete: ["<IDs>", "--search", "--force"],
+    },
+    inode: {
+      "<path>": {
+        files: {
+          list: ["--page", "--fields", "--search"],
+          fieldslist: [],
+          delete: ["<IDs>", "--search", "--force"],
+          share: ["--force"],
+        },
+        file: {
+          view: ["--type"],
+        },
       },
     },
-  },
-});
+  });
 
-completion.init();
+  completion.init();
 
-if (~process.argv.indexOf("--completion")) {
-  completion.setupShellInitFile();
+  if (~process.argv.indexOf("--completion")) {
+    completion.setupShellInitFile();
+  }
 }
 
-program
-  .option("-v, --verbose", "enable verbose output")
-  .option("-c, --config <path>", "path to config file")
-  .option("-s, --nosplash", "disable splash screen");
+async function setupOtherCommands() {
+  // Check if we have a valid ChRIS connection
+  const client = chrisConnection.getClient();
+  if (!client) {
+    console.error(
+      "Not connected to ChRIS. Please use the 'connect' command first."
+    );
+    process.exit(1);
+  }
+
+  // Setup other commands here
+  setupFileBrowserCommand(program);
+  await setupInodeCommand(program);
+
+  const pluginGroupHandler = new PluginGroupHandler();
+  pluginGroupHandler.setupCommand(program);
+
+  const pluginMetaGroupHandler = new PluginMetaGroupHandler();
+  pluginMetaGroupHandler.setupCommand(program);
+
+  const feedGroupHandler = new FeedGroupHandler();
+  feedGroupHandler.setupCommand(program);
+
+  const feedMemberHandler = new FeedMemberHandler();
+  feedMemberHandler.setupCommand(program);
+
+  // Setup command completion
+  setupCommandCompletion();
+}
 
 async function main() {
-  program.parse(process.argv);
+  // Setup basic program info
+  program
+    .option("-v, --verbose", "enable verbose output")
+    .option("-c, --config <path>", "path to config file")
+    .option("-s, --nosplash", "disable splash screen");
 
+  // Setup connect command early
+  setupConnectCommand(program);
+
+  // Parse arguments to check if it's a connect command
+  program.parseOptions(process.argv);
   const options = program.opts();
 
   if (!options.nosplash) {
@@ -101,6 +112,25 @@ async function main() {
   if (options.config) {
     console.log(`Using config file: ${options.config}`);
   }
+
+  // If it's not a connect command, setup other commands
+  if (!process.argv.includes("connect")) {
+    await setupOtherCommands();
+  }
+  program
+    .option(
+      "--context <context>",
+      "Specify the ChRIS context URL and/or unextpath"
+    )
+    .hook("preAction", (thisCommand, actionCommand) => {
+      const context = thisCommand.opts().context;
+      if (context) {
+        // Set the context for the ChRIS connection
+        chrisConnection.setContext(context);
+      }
+    });
+  // Re-parse arguments to handle the selected command
+  program.parse(process.argv);
 }
 
 main().catch((error) => {

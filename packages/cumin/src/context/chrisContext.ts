c50caf2160d9d5e52b6a389c68828b24b8f780f8
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-08-12 19:42:50 -0400

    Refactor and expand

--- packages/cumin/src/context/chrisContext.ts
+++ packages/cumin/src/context/chrisContext.ts
@@ -1,21 +1,101 @@
-import { sessionConfig } from "../config/config";
+import { sessionConfig, readFile, ConnectionConfig } from "../config/config";
+import fs from "fs";
+import path from "path";
 
-enum Context {
+export enum Context {
   ChRISURL,
   ChRISuser,
-  ChRISfilepath,
+  ChRISfolder,
   ChRISfeed,
 }
 
-export class chrisContext {
-  private URLlist: string[] | null = null;
-  private URL: string[] | null = null;
-  private userlist: string[] | null = null;
-  private user: string = "";
-  private filepath: string = "";
-  private feed: string = "";
+export interface URLContext {
+  folder: string | null;
+  feed: string | null;
+  token: string | null;
+}
 
-  constructor() {}
+export interface SingleContext extends URLContext {
+  URL: string | null;
+  user: string | null;
+}
+
+export interface UserContext {
+  urls: {
+    [url: string]: URLContext;
+  };
+  currentURL: string | null;
+}
+
+export interface FullContext {
+  users: {
+    [user: string]: UserContext;
+  };
+  currentUser: string | null;
+  currentURL: string | null;
+}
+
+export class ChrisContext {
+  private fullContext: FullContext = {
+    users: {},
+    currentUser: null,
+    currentURL: null,
+  };
+  private singleContext: SingleContext = {
+    URL: null,
+    user: null,
+    folder: null,
+    feed: null,
+    token: null,
+  };
+
+  constructor() {
+    this.initialize();
+  }
+
+  initialize(): void {
+    const configDir: string = sessionConfig.connection.configDir;
+    const users: string[] = fs
+      .readdirSync(configDir)
+      .filter((file) => fs.statSync(path.join(configDir, file)).isDirectory());
+
+    this.fullContext.currentUser = sessionConfig.connection.loadLastUser();
+    const currentURL: string | null = sessionConfig.connection.loadChrisURL();
+    this.fullContext.currentURL = currentURL;
+
+    users.forEach((user) => {
+      this.fullContext.users[user] = { urls: {}, currentURL: null };
+      const userDir: string = path.join(configDir, user);
+      const chrisURLFile = path.join(
+        userDir,
+        sessionConfig.connection.chrisURLfile
+      );
+      if (fs.existsSync(chrisURLFile)) {
+        this.fullContext.users[user].currentURL = readFile(chrisURLFile);
+      }
+      const urlDirs: string[] = fs
+        .readdirSync(userDir)
+        .filter((file: string): boolean =>
+          fs.statSync(path.join(userDir, file)).isDirectory()
+        );
+      urlDirs.forEach((urlDir: string): void => {
+        let url: string = (
+          sessionConfig.connection as ConnectionConfig
+        ).dirToUri(urlDir);
+        this.fullContext.users[user].urls[url] = {
+          folder: readFile(path.join(userDir, urlDir, sessionConfig.cwdFile)),
+          feed: readFile(path.join(userDir, urlDir, sessionConfig.feedFile)),
+          token: readFile(
+            path.join(userDir, urlDir, sessionConfig.connection.tokenFile)
+          ),
+        };
+      });
+    });
+  }
+
+  getFullContext(): FullContext {
+    return this.fullContext;
+  }
 
   ChRISURL_get(): string | null {
     return sessionConfig.connection.loadChrisURL();
@@ -25,13 +105,71 @@ export class chrisContext {
     return sessionConfig.connection.loadLastUser();
   }
 
+  ChRISfolder_get(): string | null {
+    return sessionConfig.getPathContext();
+  }
+
+  ChRISfeed_get(): string | null {
+    return sessionConfig.getFeedContext();
+  }
+
+  ChRISfolder_set(path: string): boolean {
+    return sessionConfig.setPathContext(path);
+  }
+
+  ChRISfeed_set(feedID: string): boolean {
+    return sessionConfig.setFeedContext(feedID);
+  }
+
+  get folderpath(): string | null {
+    return sessionConfig.getPathContext();
+  }
+
+  currentContext_update() {
+    this.singleContext.URL = this.ChRISURL_get();
+    this.singleContext.user = this.ChRISuser_get();
+    this.singleContext.folder = this.ChRISfolder_get();
+    this.singleContext.feed = this.ChRISfeed_get();
+  }
+
   getCurrent(context: Context): string | null {
-    let current: string | null = null;
+    this.currentContext_update();
     switch (context) {
       case Context.ChRISURL:
-        current = this.ChRISURL_get();
+        return this.singleContext.URL;
+      case Context.ChRISuser:
+        return this.singleContext.user;
+      case Context.ChRISfolder:
+        return this.singleContext.folder;
+      case Context.ChRISfeed:
+        return this.singleContext.feed;
+    }
+  }
+
+  setCurrent(context: Context, value: string): boolean {
+    let status: boolean = false;
+    switch (context) {
+      case Context.ChRISuser:
+        this.singleContext.user = value;
+        status = sessionConfig.connection.saveLastUser(value);
+        break;
+      case Context.ChRISURL:
+        this.singleContext.URL = value;
+        status = sessionConfig.connection.saveChrisURL(value);
+        break;
+      case Context.ChRISfolder:
+        this.singleContext.folder = value;
+        status = this.ChRISfolder_set(value);
+        break;
+      case Context.ChRISfeed:
+        this.singleContext.feed = value;
+        status = this.ChRISfeed_set(value);
         break;
     }
-    return current;
+    sessionConfig.connection.initialize();
+    sessionConfig.initialize();
+    return status;
   }
 }
+
+export const chrisContext: ChrisContext = new ChrisContext();

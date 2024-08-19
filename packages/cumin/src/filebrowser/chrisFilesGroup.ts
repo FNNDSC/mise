d1ca6d56465a69d650773202121c30e2461c9df9
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-08-19 13:48:08 -0400

    Expand new design for links and dirs

--- packages/cumin/src/filebrowser/chrisFilesGroup.ts
+++ packages/cumin/src/filebrowser/chrisFilesGroup.ts
@@ -1,57 +1,38 @@
-import { ChRISResourceGroup } from "../resources/chrisResourceGroup";
+import { ChRISFileSystemGroup } from "./chrisFileSystemGroup";
 import { FileBrowserFolder } from "@fnndsc/chrisapi";
-import { chrisConnection } from "../connect/chrisConnection";
-import Client from "@fnndsc/chrisapi";
 
-class ChRISConnectionError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = "ChRISConnectionError";
-  }
-}
-
-class ChRISInitializationError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = "ChRISInitializationError";
-  }
-}
-
-export class ChRISFilesGroup extends ChRISResourceGroup {
-  private _fileBrowserFolderObj: FileBrowserFolder | null = null;
-  private _path: string;
-
-  private constructor(fileBrowserFolder: FileBrowserFolder, path: string) {
-    super("Files", "getFiles", fileBrowserFolder);
-    this._path = path;
+export class ChRISFilesGroup extends ChRISFileSystemGroup {
+  constructor(fileBrowserFolder: FileBrowserFolder, path: string) {
+    super("Files", "getFiles", fileBrowserFolder, path);
   }
 
   static async create(path: string): Promise<ChRISFilesGroup> {
-    const client: Client | null = chrisConnection.getClient();
-    if (!client) {
-      throw new ChRISConnectionError("ChRIS client is not initialized");
-    }
-
-    let fileBrowserFolder: FileBrowserFolder | null;
-    try {
-      fileBrowserFolder = await client.getFileBrowserFolderByPath(path);
-    } catch (error) {
-      throw new ChRISInitializationError(
-        "Failed to get FileBrowserFolder: " +
-          (error instanceof Error ? error.message : String(error))
-      );
-    }
-
-    if (!fileBrowserFolder) {
-      throw new ChRISInitializationError(
-        "Failed to initialize FileBrowserFolder"
-      );
-    }
-
+    const fileBrowserFolder =
+      await ChRISFileSystemGroup.initializeFileBrowserFolder(path);
     return new ChRISFilesGroup(fileBrowserFolder, path);
   }
+}
 
-  public get fileBrowserFolderObj(): FileBrowserFolder | null {
-    return this._fileBrowserFolderObj;
+export class ChRISLinksGroup extends ChRISFileSystemGroup {
+  constructor(fileBrowserFolder: FileBrowserFolder, path: string) {
+    super("Links", "getLinkFiles", fileBrowserFolder, path);
+  }
+
+  static async create(path: string): Promise<ChRISLinksGroup> {
+    const fileBrowserFolder =
+      await ChRISFileSystemGroup.initializeFileBrowserFolder(path);
+    return new ChRISLinksGroup(fileBrowserFolder, path);
+  }
+}
+
+export class ChRISDirsGroup extends ChRISFileSystemGroup {
+  constructor(fileBrowserFolder: FileBrowserFolder, path: string) {
+    super("Directories", "getChildren", fileBrowserFolder, path);
+  }
+
+  static async create(path: string): Promise<ChRISDirsGroup> {
+    const fileBrowserFolder =
+      await ChRISFileSystemGroup.initializeFileBrowserFolder(path);
+    return new ChRISDirsGroup(fileBrowserFolder, path);
   }
 }

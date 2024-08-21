5aef797e385bbb9329c7882a9c6405e5d4e7de38
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-08-21 18:17:00 -0400

    Add listing of topics and fix document location resolution

--- packages/chili/src/man/man.ts
+++ packages/chili/src/man/man.ts
@@ -3,33 +3,35 @@ import fs from "fs";
 import path from "path";
 import chalk from "chalk";
 import { marked } from "marked";
-import { execSync } from "child_process";
+import { exec, execSync, ExecException } from "child_process";
+import url from "url";
+import os from "os";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { library, icon, dom } from "@fortawesome/fontawesome-svg-core";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
 
-export function setupManCommand(program: Command): void {
-  program
-    .command("man <topic>")
-    .description("Display the manual for a ChILI topic")
-    .option("-b, --browser", "Open documentation in browser")
-    .action((topic: string, options: { browser?: boolean }) => {
-      const docPath: string = path.join(
-        __dirname,
-        "..",
-        "doc",
-        `${topic}.adoc`
-      );
+library.add(faCamera);
+interface ManPageOptions {
+  topic: string;
+  browser?: boolean;
+}
 
-      if (!fs.existsSync(docPath)) {
-        console.error(`Documentation for '${topic}' not found.`);
-        return;
-      }
+const docDir: string = path.join(projectDir_get(), "doc");
 
-      if (options.browser) {
-        openInBrowser(docPath);
-      } else {
-        const content: string = fs.readFileSync(docPath, "utf-8");
-        console.log(renderAsciidoc(content));
-      }
-    });
+function projectDir_get(): string {
+  const currentFilePath: string = import.meta.url;
+  const currentDirectory: string = path.dirname(currentFilePath);
+
+  let directoryToCheck: string | null = currentDirectory;
+  while (directoryToCheck) {
+    const packageJsonPath: string = path.join(directoryToCheck, "package.json");
+    const filePath = url.fileURLToPath(packageJsonPath);
+    if (fs.existsSync(filePath)) {
+      return url.fileURLToPath(directoryToCheck);
+    }
+    directoryToCheck = path.dirname(directoryToCheck);
+  }
+  return url.fileURLToPath(currentDirectory);
 }
 
 function renderAsciidoc(content: string): string {
@@ -44,13 +46,14 @@ function renderAsciidoc(content: string): string {
 }
 
 function openInBrowser(filePath: string): void {
-  const tempHtmlPath: string = filePath.replace(".adoc", ".html");
+  const tempHtmlPath: string = path.join(
+    os.tmpdir(),
+    path.basename(filePath).replace(".adoc", ".html")
+  );
 
   try {
-    // Convert asciidoc to HTML
     execSync(`asciidoctor -o ${tempHtmlPath} ${filePath}`);
 
-    // Open the HTML file in the default browser
     const command: string =
       process.platform === "win32"
         ? "start"
@@ -63,8 +66,59 @@ function openInBrowser(filePath: string): void {
       "Error opening documentation in browser:",
       error instanceof Error ? error.message : String(error)
     );
-  } finally {
-    // Clean up the temporary HTML file
-    fs.unlinkSync(tempHtmlPath);
   }
+}
+
+async function topics_list(): Promise<string[]> {
+  library.add(faBook);
+  const bookIcon = "";
+  const files: string[] = fs.readdirSync(docDir);
+  const adocFiles: string[] = files.filter(
+    (file: string) => path.extname(file) === ".adoc"
+  );
+  const formattedOutput: string[] = adocFiles.map(
+    (file: string) => `${bookIcon} ${path.basename(file, ".adoc")}`
+  );
+  return formattedOutput;
+}
+
+function manpage_handle(options: ManPageOptions): void {
+  const docPath: string = path.join(docDir, `${options.topic}.adoc`);
+
+  if (!fs.existsSync(docPath)) {
+    console.error(`Documentation for '${options.topic}' not found.`);
+    return;
+  }
+
+  if (options.browser) {
+    openInBrowser(docPath);
+  } else {
+    const content: string = fs.readFileSync(docPath, "utf-8");
+    console.log(renderAsciidoc(content));
+  }
+}
+
+export function setupManCommand(program: Command): void {
+  const manCommand: Command = program
+    .command("man")
+    .description("ChILI built in manual and help pages");
+
+  manCommand
+    .command("doc <topic>")
+    .description("Display the manual document for a ChILI topic")
+    .option("-b, --browser", "Open documentation in browser")
+    .action((topic, options: ManPageOptions) => {
+      options.topic = topic;
+      manpage_handle(options);
+    });
+
+  manCommand
+    .command("topics")
+    .description("List the available manual page topics")
+    .action(async () => {
+      const files: string[] = await topics_list();
+      console.log("\n\nThe following topics are available:");
+      console.log("(read more with 'chili man doc <topic>'\n");
+      console.log(files.join("\n"));
+    });
 }

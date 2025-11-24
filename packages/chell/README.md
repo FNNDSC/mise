a9bea083a8278d852555961468da955608efad77
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2025-11-23 19:02:02 -0500

    feat: Initial commit for chell interactive shell

---
+++ packages/chell/README.md
@@ -0,0 +1,7 @@
+# Chell - The ChRIS Shell
+
+`chell` is an interactive command-line shell for interacting with a *ChRIS* instance.
+
+## Under Construction
+
+This project is currently under construction. It will leverage the command libraries from `chili` and the business logic from `salsa` to provide an interactive user experience.
---
+++ packages/chell/src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @file Main entry point for the Chell interactive shell.
+ */
+
+console.log("Welcome to Chell!");
+
+async function main(): Promise<void> {
+  console.log("The interactive shell is under construction.");
+  console.log("It will use the refactored commands from 'chili' and 'salsa'.");
+}
+
+main().catch(console.error);

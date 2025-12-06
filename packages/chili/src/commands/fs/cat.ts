ec4af3d2d9f9ebc6a5aedcbc3e4f0f5d505c9179
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2025-12-06 12:19:33 -0500

    refactor(cat): Update handler to propagate Result<T>
    Fix implicit typing.

--- packages/chili/src/commands/fs/cat.ts
+++ packages/chili/src/commands/fs/cat.ts
@@ -4,6 +4,7 @@
  */
 import { files_content } from "@fnndsc/salsa";
 import { path_resolveChrisFs } from "../../utils/cli.js";
+import { Result } from "@fnndsc/cumin";
 
 /**
  * Retrieves the content of a file.
@@ -11,7 +12,7 @@ import { path_resolveChrisFs } from "../../utils/cli.js";
  * @param filePath - The path to the file.
  * @returns Promise resolving to content string or null.
  */
-export async function files_cat(filePath: string): Promise<string | null> {
+export async function files_cat(filePath: string): Promise<Result<string>> {
   const resolved: string = await path_resolveChrisFs(filePath, {});
   return await files_content(resolved);
 }

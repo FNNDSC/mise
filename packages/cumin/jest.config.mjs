a252111fd240a3e242430cb4b01b90cbe994d48f
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2025-12-13 22:17:27 -0500

    chore: inline jest config (drop shared base)

--- packages/cumin/jest.config.mjs
+++ packages/cumin/jest.config.mjs
@@ -1,6 +1,21 @@
-import baseConfig from '../jest.config.base.mjs';
-
 export default {
-  ...baseConfig,
-  // Cumin-specific overrides can go here
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true, tsconfig: { module: 'ESNext' } }],
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^chalk$': '<rootDir>/__mocks__/chalk.js',
+  },
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/index.ts',
+    '!<rootDir>/src/**/*.d.ts',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
